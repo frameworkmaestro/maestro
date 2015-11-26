2b2043568a4104d567dd2b2f0228b781cba107a3
Daniel Schmitz <danieljfa@gmail.com>
2015-11-26 14:50:35 -0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set("log_errors", "on");
+ini_set("error_log", "core/var/log/php_error.log");
+
+// Diretório base do Maestro
+$dir = dirname(__FILE__);
+// Arquivo de configuração
+$conf = dirname(__FILE__) . '/core/conf/conf.php';
+
+require_once($dir . '/vendor/autoload.php');
+
+\Maestro\Manager::process($conf, $dir);
