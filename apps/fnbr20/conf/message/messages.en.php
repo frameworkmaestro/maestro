cf29069d4869ec493b18b87cf003e88cb8e541fb
Ely Edison Matos <ely.matos@gmail.com>
2016-05-06 19:52:31 -0300

    Removing fnbr20

--- apps/fnbr20/conf/message/messages.en.php
+++
@@ -1,5 +0,0 @@
-<?php
-    
-    return array(
-		'msgThroughSystem' => "<p>This message was sent through 'Framenet Brasil WebTool'.</p>", 
-    );
--- apps/fnbr20/conf/message/messages.pt.php
+++
@@ -1,5 +0,0 @@
-<?php
-    
-    return array(
-		'msgThroughSystem' => "<p>Esta mensagem foi enviada através da aplicação 'Framenet Brasil WebTool'.</p>", 
-    );
